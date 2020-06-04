a2daf5b3580059fe2739f64d8fea4e057e188fe8
theadityasc30 <aditya05prasad@gmail.com>
2020-06-04 19:08:41 -0400

    Initial Commit

---
+++ FirstDataBase/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FirstDataBase
+//
+//  Created by Aditya Prasad on 6/4/20.
+//  Copyright © 2020 Aditya Prasad. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
